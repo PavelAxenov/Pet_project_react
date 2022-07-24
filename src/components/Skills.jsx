c0b9bee1b5b648d2a16fa578bc598011a3ddb084
Pavel Axenov <avatar9722@yandex.ru>
2022-07-24 12:18:51 +0300

    update

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -7,56 +7,32 @@ import javaScript from "../images/skills/javascript.svg";
 import reAct from "../images/skills/react.svg";
 import Figma from "../images/skills/figma.svg";
 import Vue from "../images/skills/vue.svg";
-import Rang from "../images/skills/rang.svg";
 import "../css/skills.css";
 
+
 export default function Skills() {
+    let arr = [
+            html,
+            Css,
+            Sass,
+            vsCode,
+            javaScript,
+            reAct,
+            Vue,
+            Figma
+        ];
+    
+   
+    const listItems = arr.map((item) =>
+        <li key={item.toString()} className="skills__prog"><img src={item} className="prog" alt="html" /></li>
+    );
+
     return (
         <section className="skills">
             <h1>Навыки</h1>
             <span>Я работаю с такими программами как:</span>
             <ul className="skills__programms">
-                <li className="skills__prog">
-                    <img src={html} className="prog" alt="html" />
-                    <span className="prog-name"></span>
-                    <img src={Rang} className="prog-rang" alt="rang" />
-                </li>
-                <li className="skills__prog">
-                    <img src={Css} className="prog" alt="css" />
-                    <span className="prog-name"></span>
-                    <img src={Rang} className="prog-rang" alt="rang" />
-                </li>
-                <li className="skills__prog">
-                    <img src={Sass} className="prog" alt="Sass"  />
-                    <span className="prog-name"></span>
-                    <img src={Rang} className="prog-rang" alt="rang" />
-                </li>
-                <li className="skills__prog">
-                    <img src={javaScript} className="prog" alt="javaScript" />
-                    <span className="prog-name"></span>
-                    <img src={Rang} className="prog-rang" alt="rang" />
-                </li>
-                <li className="skills__prog">
-                    <img src={reAct} className="prog" alt="react" />
-                    <span className="prog-name"></span>
-                    <img src={Rang} className="prog-rang" alt="rang" />
-                </li>
-                <li className="skills__prog">
-                    <img src={Vue} className="prog" alt="vue" />
-                    <span className="prog-name"></span>
-                    <img src={Rang} className="prog-rang" alt="rang" />
-                </li>
-                <li className="skills__prog">
-                    <img src={Figma} className="prog" alt="Figma" />
-                    <span className="prog-name"></span>
-                    <img src={Rang} className="prog-rang" alt="rang" />
-                </li>
-                <li className="skills__prog">
-                    <img src={vsCode} className="prog" alt="vscode" />
-                    <span className="prog-name"></span>
-                    <img src={Rang} className="prog-rang" alt="rang" />
-                </li>
-
+                {listItems}
             </ul>
         </section>
     )
